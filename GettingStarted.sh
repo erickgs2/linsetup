aa042ffb61ca9b27279a9204bc74b71970e7a295
Erick <agratobgs@gmail.com>
2021-11-20 07:55:29 +0000

    First Commit: All set up

--- GettingStarted.sh
+++ GettingStarted.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update
 sudo apt install --yes software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
+sudo apt install snapd
+ansible-galaxy collection install community.general
+
+ansible-playbook ssh-key-installer.yml -K --ask-vault-pass -v
+ansible-playbook vsCode.yml -K -v
+ansible-playbook node.yml -K -v
