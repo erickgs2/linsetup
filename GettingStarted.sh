0edbdc4e7972945e5b41571614dc85221a25dbe5
Fawkes <agratobgs@gmail.com>
2021-11-22 01:41:55 +0000

    MySql added

--- GettingStarted.sh
+++ GettingStarted.sh
@@ -7,11 +7,15 @@ sudo apt install ansible --yes
 sudo apt install snapd
 ansible-galaxy collection install community.general
 
+sudo apt install mysql-server --yes
+sudo mysql_secure_installation
+
 ansible-playbook ssh-key-installer.yml -K --ask-vault-pass -v
 ansible-playbook vsCode.yml -K -v
 ansible-playbook node.yml -K -v
+#ansible-playbook workbench.yml -K -v
 
 git config --global user.email "agratobgs@gmail.com"
 git config --global user.name "Fawkes"
-git set remote origin git@github.com:erickgs2/linsetup.git
-
+git remote set-url origin git@github.com:erickgs2/linsetup.git
+git remote set-url origin git@github.com:erickgs2/Lerma.git
