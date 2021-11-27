0f0e455f280b7768d73bb58b392e4a2979347c0d
Fawkes <agratobgs@gmail.com>
2021-11-27 00:15:13 +0000

    Update

--- GettingStarted.sh
+++ GettingStarted.sh
@@ -5,17 +5,18 @@ sudo apt install --yes software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible --yes
 sudo apt install snapd
-ansible-galaxy collection install community.general
+#ansible-galaxy collection install community.general
+sudo snap install code --classic
 
 sudo apt install mysql-server --yes
 sudo mysql_secure_installation
 
 sudo ansible-playbook ssh-key-installer.yml -K --ask-vault-pass -v
-sudo ansible-playbook vsCode.yml -K -v
+#sudo ansible-playbook vsCode.yml -K -v
 sudo ansible-playbook node.yml -K -v
 #ansible-playbook workbench.yml -K -v
 
 sudo git config --global user.email "agratobgs@gmail.com"
 sudo git config --global user.name "Fawkes"
 sudo git remote set-url origin git@github.com:erickgs2/linsetup.git
-sudo git remote set-url origin git@github.com:erickgs2/Lerma.git
+#sudo git remote set-url origin git@github.com:erickgs2/Lerma.git
