1934668838df13101845a287a87e37102036c43d
Fawkes <agratobgs@gmail.com>
2022-01-27 04:15:47 +0000

    Docker Installation Edited

--- GettingStarted.sh
+++ GettingStarted.sh
@@ -22,18 +22,19 @@ sudo git remote set-url origin git@github.com:erickgs2/linsetup.git
 #sudo git remote set-url origin git@github.com:erickgs2/Lerma.git
 
 #-----INSTALL DOCKER----
-sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt update
 sudo apt-get install \
-apt-transport-https \
-ca-certificates \
-curl \
-gnupg-agent \
-software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu
-bionic stable"
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+   ca-certificates \
+   curl \
+   gnupg \
+   lsb-release
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ echo \
+ "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 apt-cache madison docker-ce
+sudo apt-get install docker-ce="8.06.1~ce~3-0~ubuntu" docker-ce-cli="8.06.1~ce~3-0~ubuntu" containerd.io
+sudo docker run hello-world
