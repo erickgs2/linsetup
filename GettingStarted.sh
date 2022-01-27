644ff4b6c04a79f9ad5bac7ffe11e5ed9249ebc8
Fawkes <agratobgs@gmail.com>
2022-01-27 05:20:14 +0000

    Docker Fix

--- GettingStarted.sh
+++ GettingStarted.sh
@@ -5,36 +5,33 @@ sudo apt install --yes software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible --yes
 sudo apt install snapd
-#ansible-galaxy collection install community.general
+
+#-----INSTALL VSCODE-----
 sudo snap install code --classic
 
+#-----INSTALL MYSQL-----
 #sudo apt install mysql-server --yes
 #sudo mysql_secure_installation
 
+#-----SET UP GITHUB SSH-----
 sudo ansible-playbook ssh-key-installer.yml -K --ask-vault-pass -v
-#sudo ansible-playbook vsCode.yml -K -v
+#-----INSTALL NODE-----
 sudo ansible-playbook node.yml -K -v
-#ansible-playbook workbench.yml -K -v
 
+#-----GIT CONFIGURATION-----
 sudo git config --global user.email "agratobgs@gmail.com"
 sudo git config --global user.name "Fawkes"
-sudo git remote set-url origin git@github.com:erickgs2/linsetup.git
+#sudo git remote set-url origin git@github.com:erickgs2/linsetup.git
 #sudo git remote set-url origin git@github.com:erickgs2/Lerma.git
 
 #-----INSTALL DOCKER----
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt update
-sudo apt-get install \
-   ca-certificates \
-   curl \
-   gnupg \
-   lsb-release
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
- echo \
- "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
- $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-apt-cache madison docker-ce
-sudo apt-get install docker-ce="8.06.1~ce~3-0~ubuntu" docker-ce-cli="8.06.1~ce~3-0~ubuntu" containerd.io
-sudo docker run hello-world
+sudu apt-get upgrade
+sudo apt install docker.io --yes
+sudo systemctl start docker
+sudo systemctl enable docker 
+sudo systemctl status docker 
+sudo apt install docker-compose --yes
+sudo groupadd docker 
+sudo gpasswd -a ${USER} docker 
+su - $USER
