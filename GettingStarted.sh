cd3a3b78e2bc66427db49a4b17d464fa3a9bbf44
Fawkes <agratobgs@gmail.com>
2022-01-27 03:34:25 +0000

    Add Docker instalation on the GettingStarted Script and remove the MYSQL

--- GettingStarted.sh
+++ GettingStarted.sh
@@ -8,8 +8,8 @@ sudo apt install snapd
 #ansible-galaxy collection install community.general
 sudo snap install code --classic
 
-sudo apt install mysql-server --yes
-sudo mysql_secure_installation
+#sudo apt install mysql-server --yes
+#sudo mysql_secure_installation
 
 sudo ansible-playbook ssh-key-installer.yml -K --ask-vault-pass -v
 #sudo ansible-playbook vsCode.yml -K -v
@@ -20,3 +20,20 @@ sudo git config --global user.email "agratobgs@gmail.com"
 sudo git config --global user.name "Fawkes"
 sudo git remote set-url origin git@github.com:erickgs2/linsetup.git
 #sudo git remote set-url origin git@github.com:erickgs2/Lerma.git
+
+#-----INSTALL DOCKER----
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt update
+sudo apt-get install \
+apt-transport-https \
+ca-certificates \
+curl \
+gnupg-agent \
+software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu
+bionic stable"
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
+apt-cache madison docker-ce
